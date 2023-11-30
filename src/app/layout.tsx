9e4f9f16d621ae0e4b404f1701b5742bbb1cfb33
HyeongAn <guddks84@gmail.com>
2023-11-30 15:03:42 +0900

    :sparkles: styled componenets && 🎨 layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import StyledComponentsRegistry from '../../lib/registry'
+import Layout from '../components/layout/layout'
 
 export const metadata: Metadata = {
   title: 'fleaauction',
@@ -12,7 +14,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>
+          <Layout>{children}</Layout>
+        </StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
